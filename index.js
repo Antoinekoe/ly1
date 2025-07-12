9c5ddcac225e7b58cfbcbac8fdfde81474d8951b
Antoinekoe <antoine.koe@gmail.com>
2025-07-12 18:34:59 +0200

    Fully functionnal switch between QR Code creation and link shortener - Fully functionnal link shortener for one url

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { Strategy } from "passport-local";
 import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
+import { nanoid } from "nanoid";
 
 const app = express();
 const port = 3000;
@@ -26,8 +27,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); // Serve static files
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  // Check if there are entry in DB
+  try {
+    const result = await pool.query("SELECT * FROM links");
+    if (result.rows.length > 0) {
+      const newUrl = result.rows[0].new_url;
+      console.log(newUrl);
+      res.render("index.ejs", { entryInDB: true, newUrl: newUrl });
+    } else {
+      res.render("index.ejs", { entryInDB: false });
+    }
+  } catch (error) {}
 });
 
 app.get("/login", (req, res) => {
@@ -42,7 +53,28 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/create-url", (req, res) => {});
+app.get("/:id", async (req, res) => {
+  const idRequested = req.params.id;
+  const result = await pool.query("SELECT * FROM links WHERE new_url=$1", [
+    idRequested,
+  ]);
+  const oldUrl = result.rows[0].old_url;
+  res.redirect(oldUrl);
+});
+
+app.post("/create-url", async (req, res) => {
+  const oldUrl = req.body.originalUrl;
+  const newUrl = nanoid(6);
+  try {
+    const result = await pool.query(
+      "INSERT INTO links (old_url, new_url) VALUES ($1, $2)",
+      [oldUrl, newUrl]
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error inserting data in DB :", error);
+  }
+});
 
 app.post("/create-qr-code", (req, res) => {});
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,56 @@
-document
-  .getElementById("shortenUrl")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("shortenUrl").classList.add("button-blue");
-    document.getElementById("createQrCode").classList.remove("button-blue");
-    document.getElementById("shortenUrlForm").style.display = "";
-    document.getElementById("createQrCodeForm").style.display = "none";
-  });
-document
-  .getElementById("createQrCode")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("shortenUrl").classList.remove("button-blue");
-    document.getElementById("createQrCode").classList.add("button-blue");
-    document.getElementById("shortenUrlForm").style.display = "none";
-    document.getElementById("createQrCodeForm").style.display = "";
-  });
+if (entryInDB === true) {
+  // Handle "Raccourcir lien" button click: show URL form, hide QR form, update button styles
+  document
+    .getElementById("shortenUrl")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("shortenUrl").classList.add("button-blue");
+      document.getElementById("createQrCode").classList.remove("button-blue");
+      document.getElementById("createQrCodeFormResult").classList.add("hidden");
+      document
+        .getElementById("shortenUrlFormResult")
+        .classList.remove("hidden");
+      document.getElementById("createQrCodeForm").classList.add("hidden");
+      document.getElementById("shortenUrlForm").classList.add("hidden");
+    });
 
+  // Handle "Créer QR Code" button click: show QR form, hide URL form, update button styles
+  document
+    .getElementById("createQrCode")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("shortenUrl").classList.remove("button-blue");
+      document.getElementById("createQrCode").classList.add("button-blue");
+      document
+        .getElementById("createQrCodeFormResult")
+        .classList.remove("hidden");
+      document.getElementById("shortenUrlFormResult").classList.add("hidden");
+    });
+} else {
+  // Handle "Raccourcir lien" button click: show URL form, hide QR form, update button styles
+  document
+    .getElementById("shortenUrl")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("shortenUrl").classList.add("button-blue");
+      document.getElementById("createQrCode").classList.remove("button-blue");
+      document.getElementById("createQrCodeForm").classList.add("hidden");
+      document.getElementById("shortenUrlForm").classList.remove("hidden");
+    });
+
+  // Handle "Créer QR Code" button click: show QR form, hide URL form, update button styles
+  document
+    .getElementById("createQrCode")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("shortenUrl").classList.remove("button-blue");
+      document.getElementById("createQrCode").classList.add("button-blue");
+      document.getElementById("createQrCodeForm").classList.remove("hidden");
+      document.getElementById("shortenUrlForm").classList.add("hidden");
+    });
+}
+
+// Show hamburger menu (mobile)
 document
   .getElementById("hamburger-menu-not-open")
   .addEventListener("click", function () {
@@ -25,6 +59,7 @@ document
     document.getElementById("hidden-menu").classList.remove("hidden");
   });
 
+// Hide hamburger menu (mobile)
 document
   .getElementById("hamburger-menu-open")
   .addEventListener("click", function () {
@@ -34,3 +69,17 @@ document
     document.getElementById("hamburger-menu-open").classList.add("hidden");
     document.getElementById("hidden-menu").classList.add("hidden");
   });
+
+// Copy short URL to clipboard when "Copier" button is clicked
+document.getElementById("copy").addEventListener("click", function () {
+  var input = document.getElementById("shorturl");
+  input.select();
+  input.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(input.value).then(() => {
+    this.textContent = "Copié !";
+    setTimeout(() => {
+      this.innerHTML =
+        "<i class='bxr  bxs-copy' style='color:#838383'></i> Copier";
+    }, 1500);
+  });
+});
